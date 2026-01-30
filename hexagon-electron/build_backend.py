4dce1b00b3c9554afc779855be459a5e2541ec9b
B-N-D <paudelbrindal80@gmail.com>
2026-01-30 22:04:38 +0545

    Fix: Exclude TensorFlow and other heavy unused libraries from PyInstaller
    - TensorFlow is in requirements but not actually used
    - Excluding it prevents build timeout on GitHub Actions
    - Also excluded other heavy GUI/notebook libraries

--- hexagon-electron/build_backend.py
+++ hexagon-electron/build_backend.py
@@ -147,7 +147,12 @@ a = Analysis(
     hookspath=[],
     hooksconfig={{}},
     runtime_hooks=[],
-    excludes=['tkinter'],
+    excludes=[
+        'tkinter', 'matplotlib.tests', 'numpy.tests',
+        'tensorflow', 'tensorboard', 'keras',
+        'IPython', 'notebook', 'jupyter',
+        'PIL.ImageQt', 'PyQt5', 'PyQt6',
+    ],
     win_no_prefer_redirects=False,
     win_private_assemblies=False,
     cipher=block_cipher,
